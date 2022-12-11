e6e8a0495f86f5582f70ce2927c84c8946d6cc42
Jason Edson <jaysonedson@gmail.com>
2022-12-10 20:58:01 -0800

    Settings: Sync Security Hub pref with pixel-framework
    * Every resources are located under vendor/pixel-framework
      So we don't need to add those here.
    
    * Instead, we will change AOSP Settings' code to correctly reflect pixel-framework's changes.
    
    Original commit: "Settings: Add Security Hub from Pixel devices"
    https://github.com/BenzoRom/packages_apps_Settings/commit/8a3ca46f7ee4bc0535c3fcafcdfd450a0b22a471
    
    Change-Id: Ifd8eb0d5660ef7072b78b23dce75325dd5b0cf64

--- src/com/android/settings/core/gateway/SettingsGateway.java
+++ src/com/android/settings/core/gateway/SettingsGateway.java
@@ -183,6 +183,8 @@ import com.android.settings.livedisplay.LiveDisplaySettings;
 
 import ink.kscope.settings.wifi.tether.WifiTetherClientManager;
 
+import com.google.android.settings.security.SecurityHubDashboard;
+
 public class SettingsGateway {
 
     /**
@@ -351,7 +353,8 @@ public class SettingsGateway {
             TurnScreenOnSettings.class.getName(),
             TurnScreenOnDetails.class.getName(),
             LiveDisplaySettings.class.getName(),
-            WifiTetherClientManager.class.getName()
+            WifiTetherClientManager.class.getName(),
+            SecurityHubDashboard.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
