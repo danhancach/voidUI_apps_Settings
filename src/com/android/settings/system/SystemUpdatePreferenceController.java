VoidUI/packages_apps_Settings

Settings: Adapt for VoidUI Project
Change-Id: I11d46cf09487be7aa9cbd6bccd564141cfd6a685

src/com/android/settings/system/SystemUpdatePreferenceController.java
@@ -34,8 +34,8 @@ public class SystemUpdatePreferenceController extends BasePreferenceController {
 
     private static final String KEY_SYSTEM_UPDATE_SETTINGS = "system_update_settings";
 
-    private static final String OTA_BUILD_TYPE_PROP = "org.pixelexperience.build_type";
-    private static final String OTA_APP_PACKAGE = "org.pixelexperience.ota";
+    private static final String OTA_BUILD_TYPE_PROP = "org.voidui.build_type";
+    private static final String OTA_APP_PACKAGE = "org.voidui.ota";
 
     private final UserManager mUm;
 

danhancach/voidUI_apps_Settings

src/com/android/settings/system/SystemUpdatePreferenceController.java
@@ -34,8 +34,8 @@ public class SystemUpdatePreferenceController extends BasePreferenceController {
 
     private static final String KEY_SYSTEM_UPDATE_SETTINGS = "system_update_settings";
 
-    private static final String OTA_BUILD_TYPE_PROP = "org.pixelexperience.build_type";
-    private static final String OTA_APP_PACKAGE = "org.pixelexperience.ota";
+    private static final String OTA_BUILD_TYPE_PROP = "org.voidui.build_type";
+    private static final String OTA_APP_PACKAGE = "com.voidui.updater";
 
     private final UserManager mUm;
 